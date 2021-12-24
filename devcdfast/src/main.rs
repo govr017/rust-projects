d70376f46ce26d43b60a1978303685beb9280afb
Gavrilov Yaroslav <govr017@gmail.com>
2021-12-24 21:59:10 +0500

    (devcdfast) Updated code to add new directory + fix .gitingore

--- devcdfast/src/main.rs
+++ devcdfast/src/main.rs
@@ -1,7 +1,7 @@
 use std::io;
 fn main() {
     let mut answer = String::new();
-    println!("Choose your destination: dev,rust,projects");
+    println!("Choose your destination: dev,rust,projects(obs),projects-rust");
     io::stdin().read_line(&mut answer).expect("Failed to read input");
     if answer.to_string().trim() == "dev" {
         println!("cd /mnt/i/Documents/dev/");
@@ -11,5 +11,8 @@ fn main() {
     }
     else if answer.to_string().trim() == "projects" {
         println!("cd /mnt/i/Documents/dev/rust/projects/")
+    }
+    else if answer.to_string().trim() == "projects-rust" {
+        println!("cd /mnt/i/Documents/rust-projects/")
     }
 }
